487a090325fbe5c157cdbce92601b7f7503b73e4
Bachar Daowd <bachar.daowd@gmail.com>
2023-11-07 07:29:27 +0100

    Creating the hello endpoint for testing authentication

--- src/main/java/com/bachar/recipes/user/controller/LoginController.java
+++ src/main/java/com/bachar/recipes/user/controller/LoginController.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -27,6 +28,11 @@ public class LoginController {
     @PostMapping("/api/1.0/login")
     public LoginResponse handleLogin(@RequestBody @Validated LoginShem user) {
        return authService.attemptLogin(user);
+    }
+
+    @GetMapping("/api/1.0/hello")
+    public String handleLogin(@AuthenticationPrincipal UserPrincipal userPrincipal) {
+        return "If you can see this message that means that you "+ userPrincipal.getUsername() + " are authenticated";
     }
 
 }
